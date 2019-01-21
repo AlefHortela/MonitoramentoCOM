674073c3807a358ea8da31b382e0a7f7bbe2876b
Sergio Rodrigues da Silva <sergiosilva@fitec.org.br>
2019-01-21 09:12:01 -0200

    Alterado posição do label

--- TestConnect/TestConnect/TestConnect.Designer.cs
+++ TestConnect/TestConnect/TestConnect.Designer.cs
@@ -77,7 +77,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(156, 24);
+            this.label1.Location = new System.Drawing.Point(157, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 4;
